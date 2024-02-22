b019c4690326bf48a2feb280c340e089ecc23664
rupeshkumar8825 <therupeshkumar8825@gmail.com>
2024-02-22 23:39:24 +0530

    Feature : implement the feature to be able to download the repo on the server with unique id

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import simpleGit from 'simple-git';
+import { generateId } from './utils';
 
 
 const app = express();
@@ -12,6 +14,16 @@ app.post("/deploy", (req : Request, res : Response ) => {
     console.log(repoUrl);
     // now here we have to write the lo1gic for cloning this repo for this purpose 
     // we will use the simple git for this purpose 
+    // now we have to clone the repo here using the simple git for this purpose 
+    // below handles the promise that is being returned by this function and hence we have to write the callback functions  or we can just did await and async for this purpose 
+    // fetching the id for this repo 
+    const currRepoId = generateId();
+    console.log("the unique id for this project is as follwos \n", currRepoId);
+    simpleGit().clone(repoUrl, `./output/${currRepoId}`).then(() => {
+        console.log("successfully cloned the repo for this purpose \n");
+    }).catch(err => {
+        console.log("some error happened while cloning the repo for this purpose: ", err);
+    });
 
     res.json({repoUrl});
 
