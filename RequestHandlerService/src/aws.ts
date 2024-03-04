ba7956d226264b1ca967d591b77aae64e0d5ea77
rupeshkumar8825 <therupeshkumar8825@gmail.com>
2024-03-04 21:56:11 +0530

    Feature: Done function to fetch the content from the S3 bucket from cloudflare

---
+++ RequestHandlerService/src/aws.ts
@@ -0,0 +1,36 @@
+import aws from "aws-sdk";
+
+// initializing the s3 object here for this puropse 
+const s3 = new aws.S3({
+    accessKeyId: "6ec32fa33747e375e05fc2a279d1ab72", 
+    secretAccessKey: "d3eda2528c601ee9162badf4c9682d8f3adcfd9844bc4bf1dc0e5bdab4668715", 
+    endpoint: "https://1ea20514e3014ab5d376bc593ece2c61.r2.cloudflarestorage.com"
+});
+
+
+
+// now we have to define the function using which we will be able to fetch the files from the S3 bucket and we will be able to serve it here for this purpose 
+const getFileFromServer = (filePath : string, deploymentId : string) => {
+    // this will itself will send the response back and this function uses the async await for this purpose 
+    return new Promise((resolve , reject) => {
+        s3.getObject({
+            Bucket : "vercel-clone", 
+            Key : `build/${deploymentId}${filePath}`
+        }, (err, response) => {
+            if(err)
+            {
+                console.log("some error happened while accessing the file from the S3 bucket", err);
+                resolve(err);
+            }
+            else
+            {
+                console.log("the response from the S3 bucket is as follows \n", response);
+                resolve(response)
+            }
+        });
+
+    }) 
+}
+
+
+export default getFileFromServer;
--- RequestHandlerService/src/index.ts
+++ RequestHandlerService/src/index.ts
@@ -2,3 +2,38 @@
 
 
 console.log("this is index.ts for request handler for this purpose \n");
+import express, { Request, Response } from "express";
+import getFileFromServer from "./aws";
+
+const app = express();
+
+
+
+app.get("/*", async (req : Request,res : Response) => {
+    console.log("this endpoint was hit for this purpose \n");
+    // now fetching the value of the hostname 
+    const url = req.hostname;
+    console.log("host is \n", url);
+
+    const id = url.split(".")[0];
+
+    console.log("the value of id is as follows \n", id);
+    
+    const requestedPath = req.path;
+    console.log("the request path is as follows \n", requestedPath);
+
+    // now we have to write the logic in order to fetch the file from the S3 to local and then serve this file for this purpose 
+    const response = await getFileFromServer(requestedPath, id);
+
+    console.log("the response from the server is as follows \n", response);
+    console.log("sending the json response to the frontend for this purpose \n");
+    res.json({response});
+
+})
+
+
+
+
+app.listen(3001, () => {
+    console.log(`server is running on port 3001 `);
+});
